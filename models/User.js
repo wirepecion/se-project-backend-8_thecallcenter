e5601b35d1f730ca5bc8162152549698b6c47f30
Worachart Poungtabtim <wrchxrt4712@gmail.com>
2025-02-26 15:28:52 +0700

    pond

--- models/User.js
+++ models/User.js
@@ -7,6 +7,10 @@ const UserSchema=new mongoose.Schema({
         type:String,
         required:[true,'Please add a name']
     },
+    tel:{
+        type: String,
+        required:[true,'Please add a telephone number']
+    },
     email:{
         type: String,
         required:[true,'Please add an email'],
