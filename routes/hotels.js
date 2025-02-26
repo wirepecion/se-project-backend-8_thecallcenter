01d1de66601c3e1e96b9f621f7e145014b353fb6
Worachart Poungtabtim <wrchxrt4712@gmail.com>
2025-02-26 13:56:11 +0700

    refile

---
+++ routes/hotels.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { getHotels, getHotel, createHotel, updateHotel, deleteHotel } = require('../controllers/hotels');
+const { protect, authorize } = require('../middleware/auth');
+
+// Include other resource routers
+const bookingsRouter = require('./bookings');
+
+const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:hotelId/bookings', bookingsRouter);
+
+router.route('/').get(getHotels).post(protect, authorize('admin'), createHotel);
+router.route('/:id').get(getHotel).put(protect, authorize('admin'), updateHotel).delete(protect, authorize('admin'), deleteHotel);
+
+module.exports = router;
