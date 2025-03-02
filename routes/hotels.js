a067066a666d05581ed4f1641f26b409cbd80eab
PatcharaponOngkakul <titanbd52@gmail.com>
2025-03-02 12:06:50 +0700

    Add rooms router to hotels route for improved resource management

--- routes/hotels.js
+++ routes/hotels.js
@@ -4,11 +4,13 @@ const { protect, authorize } = require('../middleware/auth');
 
 //Incluse other resource routers
 const bookingsRouter = require('./bookings');
+const roomsRouter = require('./rooms');
 
 const router = express.Router();
 
 //Re-route into other resource routers
 router.use('/:hotelId/bookings', bookingsRouter);
+router.use('/:hotelId/rooms', roomsRouter);
 
 router.route('/').get(getHotels).post(protect, authorize('admin'), createHotel);
 router.route('/:id').get(getHotel).put(protect, authorize('admin'), updateHotel).delete(protect, authorize('admin'), deleteHotel);
