318538a6eeef36f1ad28ba9866e07c433d25ea0d
Worachart Poungtabtim <wrchxrt4712@gmail.com>
2025-03-24 13:41:05 +0700

    Refactor server.js for improved code consistency and enable CORS with specific options

--- server.js
+++ server.js
@@ -4,47 +4,56 @@ const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-const {xss} = require('express-xss-sanitizer');
+const { xss } = require('express-xss-sanitizer');
 const hpp = require('hpp');
 const cors = require('cors');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 
 // Load env vars
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 connectDB();
 
 // Route files
-const hotels = require ('./routes/hotels');
+const hotels = require('./routes/hotels');
 const bookings = require('./routes/bookings');
 const reviews = require('./routes/reviews');
 const rooms = require('./routes/rooms');
 const payments = require('./routes/payments');
 const auth = require('./routes/auth');
 const rateLimit = require('express-rate-limit');
-    
-const app=express();
 
-//Rate limiting
+const app = express();
+
+// Rate limiting
 const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000, //10 mins
-    max: 100
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
 });
 
-const swaggerOptions={
-    swaggerDefinition:{
+const corsOptions = {
+    origin: [
+      'http://localhost:3000',  // Localhost (development)
+      'https://hotel-booking-backend-psi.vercel.app',  // Vercel domain (production)
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+const swaggerOptions = {
+  swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-    title: 'Library API',
-    version: '1.0.0',
-    description: 'A simple Express VacQ API'
+      title: 'Library API',
+      version: '1.0.0',
+      description: 'A simple Express VacQ API'
     }
-    },
-    apis:['./routes/*.js'],
+  },
+  apis: ['./routes/*.js'],
 };
-const swaggerDocs=swaggerJsDoc(swaggerOptions);
 
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 // Body parser
 app.use(express.json());
@@ -52,39 +61,22 @@ app.use(mongoSanitize());
 app.use(helmet());
 app.use(xss());
 app.use(limiter);
-app.use('/api/v1/hotels',hotels);
-app.use('/api/v1/bookings',bookings);
-app.use('/api/v1/reviews',reviews);
-app.use('/api/v1/rooms',rooms);
-app.use('/api/v1/payments',payments);
-app.use('/api/v1/auth',auth);
+app.use('/api/v1/hotels', hotels);
+app.use('/api/v1/bookings', bookings);
+app.use('/api/v1/reviews', reviews);
+app.use('/api/v1/rooms', rooms);
+app.use('/api/v1/payments', payments);
+app.use('/api/v1/auth', auth);
 
-//Prevent http param pollution
+// Prevent HTTP param pollution
 app.use(hpp());
 
-//Enable CORS
-app.use(cors());
+// Enable CORS
+app.use(cors(corsOptions));
 
 // Cookie parser
 app.use(cookieParser());
 
-app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-const PORT=process.env.PORT || 5000;
-
-// const server = app.listen (PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port', PORT));
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err,promise)=>{
-    console.log(`Error: ${err.message}`); 
-    //Close server & exit process 
-    server.close(()=>process.exit(1));
-});
-
-if (process.env.NODE_ENV !== "production") {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  }
-  
-  module.exports = app;
+module.exports = app;
