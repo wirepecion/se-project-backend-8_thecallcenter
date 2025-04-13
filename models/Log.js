6067b5e56f705ae129ab72816d5192c9de7f2b85
ahpu9158 <titiporn02468@gmail.com>
2025-04-14 00:35:53 +0700

    feat: log feature and add refund log

---
+++ models/Log.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const LogSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    type: {
+        type: String,
+        enum: ['REFUND', 'PAYMENT', 'WARNING'],
+        required: true
+    },
+
+    action: {
+        type: String,
+        required: true
+    },
+
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Log', LogSchema);
