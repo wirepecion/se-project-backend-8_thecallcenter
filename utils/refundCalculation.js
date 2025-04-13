5e78c927f5343d2915a17eb47a46b23e40222214
ahpu9158 <titiporn02468@gmail.com>
2025-04-13 18:57:24 +0700

    impement refund flow

--- utils/refundCalculation.js
+++ utils/refundCalculation.js
@@ -4,4 +4,5 @@ exports.refundCalculation = (CheckInDate, CheckOutDate, CancellationDate, paymen
     console.log("CheckOutDate: ", CheckOutDate); 
     console.log("CancellationDate: ", CancellationDate);
     console.log("paymentAmount: ", paymentAmount);
+    return paymentAmount;
 }
