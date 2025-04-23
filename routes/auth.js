4421ee89aec536d3599047dcc6c9c5809a1baab2
ahpu9158 <titiporn02468@gmail.com>
2025-04-23 08:09:51 +0700

    Implement reduceCredit function to deduct user credit with validation checks

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express= require('express');
-const {register, login, getMe, logout}=require('../controllers/auth');
+const {register, login, getMe, logout, reduceCredit}=require('../controllers/auth');
 
 const router=express.Router();
 
@@ -9,6 +9,7 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.get('/logout', logout);
+router.post('/reduceCredit', protect, reduceCredit);
 
 module.exports=router;
 
