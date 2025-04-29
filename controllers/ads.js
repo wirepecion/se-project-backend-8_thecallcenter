33f7b0135565743e187e0206ad2b61d3a537df5a
PatcharaponOngkakul <6733170321@student.chula.ac.th>
2025-04-29 12:06:49 +0700

    Refactor randomBanners function to improve banner selection logic and update subscription rank; remove min/max constraints from subscriptionRank in Hotel model

--- controllers/ads.js
+++ controllers/ads.js
@@ -1,8 +1,8 @@
 const Hotel = require('../models/Hotel');
 
-//@desc     Get all hotels
-//@route    GET /api/v1/ads
-//@access   Public
+// @desc     Get all hotels
+// @route    GET /api/v1/ads
+// @access   Public
 exports.randomBanners = async (req, res, next) => {
   try {
     const hotels = await Hotel.find();
@@ -18,30 +18,46 @@ exports.randomBanners = async (req, res, next) => {
     let banners = [];
 
     for (let i = 0; i < numberOfBanners; i++) {
+      let totalToken = 0;
+      let accBuffer = [];
 
-        let totalToken = 0;
-        let accBuffer = [];
-        for (let j = 0; j < hotels.length; j++) {
-            if (banners.includes(hotels[j].name)) continue;
+      for (let j = 0; j < hotels.length; j++) {
+        if (banners.includes(hotels[j].name)) continue;
 
-            totalToken += hotels[j].subscriptionRank;
-            accBuffer.push({ name: hotels[j].name, token: totalToken });
+        totalToken += hotels[j].subscriptionRank;
+        accBuffer.push({ name: hotels[j].name, token: totalToken });
+      }
+
+      const randomIndex = Math.floor(Math.random() * totalToken);
+      for (let j = 0; j < accBuffer.length; j++) {
+        if (accBuffer[j].token > randomIndex) {
+          const selectedName = accBuffer[j].name;
+          banners.push(selectedName);
+      
+          // Find the hotel in the hotels array
+          const selectedHotel = hotels.find(hotel => hotel.name === selectedName);
+      
+          if (selectedHotel && selectedHotel.subscriptionRank > 1) {
+            await Hotel.updateOne(
+              { name: selectedName },
+              { $inc: { subscriptionRank: -1 } }
+            );
+          }
+      
+          break;
         }
-
-        const randomIndex = Math.floor(Math.random() * totalToken);
-        for (let j = 0; j < accBuffer.length; j++) {
-            if (accBuffer[j].token > randomIndex) {
-                banners.push(accBuffer[j].name);
-                break;
-            }
-        }
+      }
+      
     }
 
-    const hotelฺBanners = await Hotel.find({name: { $in: banners }}).select('name picture address tel');
+    const hotelBanners = await Hotel.find({ name: { $in: banners } })
+      .select('name picture address tel');
+
+    console.log("Selected hotel banners:", hotelBanners.map(h => h.name));
 
     res.status(200).json({
       success: true,
-      data: hotelฺBanners,
+      data: hotelBanners,
     });
 
   } catch (err) {
